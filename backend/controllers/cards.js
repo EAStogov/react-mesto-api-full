c0305796b87f1aedf674efaa40cf71815f31d343
EgorStogov <egst94@yandex.ru>
2022-07-15 02:13:34 +0300

    refactor: refactor deletecard

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -45,7 +45,7 @@ const deleteCard = (req, res, next) => {
       if (JSON.stringify(req.user._id) !== JSON.stringify(card.owner)) {
         next(new Forbidden('Удалять чужую карточку запрещено'));
       }
-      Card.findOneAndRemove(card)
+      Card.deleteMany(card)
         .then((deletedCard) => res.send({ data: deletedCard }))
         .catch((err) => {
           if (err.name === 'CastError') {
