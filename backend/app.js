df82a7b9c861fecd7097cefb7a658efd6c1ae6be
EgorStogov <egst94@yandex.ru>
2022-07-15 08:52:21 +0300

    refactor: cors

--- backend/app.js
+++ backend/app.js
@@ -11,18 +11,14 @@ const { errors } = require('celebrate');
 const cors = require('cors');
 
 const appRouter = require('./routes/appRouter');
-// const urls = require('./constants/constants');
+const urls = require('./constants/constants');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(cors({
-  origin: [
-    'http://esto.mesto.nomoredomains.xyz',
-    'https://esto.mesto.nomoredomains.xyz',
-    'localhost:3000',
-  ],
+  origin: urls,
   credentials: true,
 }));
 
--- backend/constants/constants.js
+++ backend/constants/constants.js
@@ -1,6 +1,8 @@
 const urls = [
   'http://esto.mesto.nomoredomains.xyz',
   'https://esto.mesto.nomoredomains.xyz',
+  'http://api.esto.mesto.nomoredomains.xyz',
+  'https://api.esto.mesto.nomoredomains.xyz',
   'localhost:3000',
 ];
 
