6bac3408d5eb6d7a89f06a67b53aae07755e1841
EgorStogov <egst94@yandex.ru>
2022-07-15 03:26:26 +0300

    refactor: refactor readme & urls for cors

--- backend/constants/constants.js
+++ backend/constants/constants.js
@@ -2,6 +2,7 @@ const urls = [
   'http://esto.mesto.nomoredomains.xyz',
   'https://esto.mesto.nomoredomains.xyz',
   'http://localhost:3000',
+  'https://localhost:3000',
 ];
 
 module.exports = urls;
