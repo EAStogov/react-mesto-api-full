8cd7701a4de22bbbb16796bba1269c9ce3407d9b
EgorStogov <egst94@yandex.ru>
2022-07-16 16:13:18 +0300

    refactor: change secret

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
+const secret = NODE_ENV === 'production' ? JWT_SECRET : 'secret_key';
 
 module.exports = (req, res, next) => {
   const { cookies } = req;
@@ -15,7 +16,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, secret);
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
