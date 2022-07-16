3a8b8284984a8fdba594e710398f2986e6f72a59
EgorStogov <egst94@yandex.ru>
2022-07-16 18:14:48 +0300

    refactor: change error's handler

--- backend/app.js
+++ backend/app.js
@@ -46,7 +46,13 @@ app.use(errorLogger);
 app.use(errors());
 
 // eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => res.status(err.statusCode).send({ message: err.message }));
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
+
+  res.status(statusCode).send({ message });
+  next();
+});
 
 app.listen(PORT, () => {
 });
